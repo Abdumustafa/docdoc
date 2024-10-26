0ff3afc29f9b7dcc790d14ab4fd55d9b3bcbc0c3
Abdumustafa <www.abdumustafa123a@gmail.com>
2024-10-26 13:27:12 +0300

    some Changies

--- lib/feutures/onbording/widget/get_started_buttom.dart
+++ lib/feutures/onbording/widget/get_started_buttom.dart
@@ -14,12 +14,12 @@ class GetStartedButtom extends StatelessWidget {
           context.pushName(Routes.loginScreen);
         },
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(ColorsManager.mainBlue),
+          backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
           tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          minimumSize: MaterialStateProperty.all(
+          minimumSize: WidgetStateProperty.all(
             const Size(double.infinity, 52),
           ),
-          shape: MaterialStateProperty.all(
+          shape: WidgetStateProperty.all(
             RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
