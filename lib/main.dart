5cf12605352c82c243065b1ceb57cd1b3f312ee5
Abdumustafa <www.abdumustafa123a@gmail.com>
2024-11-16 14:36:34 +0200

    add flavorse

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:docdoc/core/routing/app_router.dart';
-import 'package:docdoc/doc_app.dart';
-import 'package:flutter/material.dart';
-import 'core/di/dependency_injection.dart';
-
-void main() {
-  setupGetIt();
-  runApp( DocApp(appRouter: AppRouter(),),);
-}
